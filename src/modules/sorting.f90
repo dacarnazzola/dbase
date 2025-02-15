33059e701851720077214089de0b9f3107c618d7
Derick Carnazzola <maxediv@gmail.com>
2025-02-14 21:52:15 -0600

    added subroutine version of qsort and isort

--- src/modules/sorting.f90
+++ src/modules/sorting.f90
@@ -7,7 +7,9 @@ private
         module procedure :: is_sorted_i32
     end interface is_sorted
 
-    public :: is_sorted, qsort, isort, isort2, isort3, qsort2, msort
+    integer(kind=i64), parameter :: isort_cutoff = 64_i64
+
+    public :: is_sorted, qsort, isort, isort2, isort3, qsort2, msort, msort2, msort3, qsort3, qsorts
 
 contains
 
@@ -30,29 +32,30 @@ contains
         integer(kind=i32) :: val(size(x, kind=i64))
         integer(kind=i64) :: n
         n = size(x, kind=i64)
-        if (n > 2048_i64) then
+        if (n > isort_cutoff) then
             val = [qsort(pack(x, x < x(1_i64))), &
                    x(1_i64), &
                    qsort(pack(x(2_i64:n), x(2_i64:n) >= x(1_i64)))]
         else
-            val = isort2(x)
+            val = isort3(x)
         end if
     end function qsort
 
     pure recursive function qsort2(x) result(val)
         integer(kind=i32), intent(in) :: x(:)
         integer(kind=i32) :: val(size(x, kind=i64))
-        integer(kind=i64) :: n, u, v, w
+        integer(kind=i64) :: n
+        integer(kind=i32) :: u, v, w
         n = size(x, kind=i64)
-        if (n <= 2048_i64) then
-            val = isort2(x)
+        if (n <= isort_cutoff) then
+            val = isort3(x)
         else
             val = x
             u = val(1)
             v = val(n/2)
             w = val(n)
             if ((v > u) .neqv. (v > w)) then !! v is median
-                val(1) = v
+                val(1_i64) = v
                 val(n/2) = u
             else if ((w > u) .neqv. (w > v)) then !! w is median
                 val(1) = w
@@ -64,43 +67,202 @@ contains
         end if
     end function qsort2
 
+    pure recursive function qsort3(x) result(val)
+        integer(kind=i32), intent(in) :: x(:)
+        integer(kind=i32) :: val(size(x, kind=i64))
+        integer(kind=i64) :: n, i
+        integer(kind=i32) :: u, v, w
+        n = size(x, kind=i64)
+        if (n <= isort_cutoff) then
+            val = isort3(x)
+        else
+            val = x
+            u = val(1)
+            v = val(n/2)
+            w = val(n)
+            if ((v > u) .neqv. (v > w)) then !! v is median
+                val(1_i64) = v
+                val(n/2_i64) = u
+            else if ((w > u) .neqv. (w > v)) then !! w is median
+                val(1) = w
+                val(n) = u
+            end if !! u is median
+            v = 1_i64
+            do i=2_i64,n
+                if (val(i) < val(1_i64)) then
+                    v = v + 1_i32
+                    u = val(i)
+                    val(i) = val(v)
+                    val(v) = u
+                end if
+            end do
+            u = val(v)
+            val(v) = val(1_i64)
+            val(1_i64) = u
+            val(1_i64:v-1_i64) = qsort3(val(1_i64:v-1_i64))
+            val(v+1_i64:n) = qsort3(val(v+1_i64:n))
+        end if
+    end function qsort3
+
+    pure recursive subroutine qsorts(x)
+        integer(kind=i32), intent(inout) :: x(:)
+        integer(kind=i64) :: n, i
+        integer(kind=i32) :: u, v, w
+        n = size(x, kind=i64)
+        if (n <= isort_cutoff) then
+            call isorts(x)
+        else
+            u = x(1)
+            v = x(n/2)
+            w = x(n)
+            if ((v > u) .neqv. (v > w)) then !! v is median
+                x(1) = v
+                x(n/2) = u
+            else if ((w > u) .neqv. (w > v)) then !! w is median
+                x(1) = w
+                x(n) = u
+            end if !! u is median
+            v = 1_i32
+            do i=2_i64,n
+                if (x(i) < x(1_i64)) then
+                    v = v + 1_i32
+                    u = x(i)
+                    x(i) = x(v)
+                    x(v) = u
+                end if
+            end do
+            u = x(v)
+            x(v) = x(1_i64)
+            x(1_i64) = u
+            call qsorts(x(1_i64:v-1_i64))
+            call qsorts(x(v+1_i64:n))
+        end if
+    end subroutine qsorts
+
     pure recursive function msort(x) result(val)
         integer(kind=i32), intent(in) :: x(:)
         integer(kind=i32) :: val(size(x, kind=i64))
-        integer(kind=i64) :: n, nl, nr, il, ir, ii, left(size(x, kind=i64)/2_i64), right(size(x, kind=i64)/2_i64+1_i64)
+        integer(kind=i64) :: n, nl, nr, il, ir, ii
+        integer(kind=i32) :: left(size(x, kind=i64)/2_i64), right(size(x, kind=i64)/2_i64+1_i64)
         n = size(x, kind=i64)
-        if (n <= 512_i64) then
+        if (n <= isort_cutoff) then
             val = isort3(x)
         else
             nl = n/2_i64
             nr = n - nl
-            left = msort(x(1:nl))
-            right = msort(x(nl+1:n))
-            il = 1
-            ir = 1
-            ii = 1
+            left = msort(x(1_i64:nl))
+            right = msort(x(nl+1_i64:n))
+            il = 1_i64
+            ir = 1_i64
+            ii = 1_i64
             do while ((il <= nl) .and. (ir <= nr))
                 if (left(il) <= right(ir)) then
                     val(ii) = left(il)
-                    il = il + 1
+                    il = il + 1_i64
                 else
                     val(ii) = right(ir)
-                    ir = ir + 1
+                    ir = ir + 1_i64
                 end if
-                ii = ii + 1
+                ii = ii + 1_i64
             end do
             do while (il <= nl)
                 val(ii) = left(il)
-                il = il + 1
-                ii = ii + 1
+                il = il + 1_i64
+                ii = ii + 1_i64
             end do
             do while (ir <= nr)
                 val(ii) = right(ir)
-                ir = ir + 1
-                ii = ii + 1
+                ir = ir + 1_i64
+                ii = ii + 1_i64
             end do
         end if
     end function msort
+
+    pure recursive function msort2(x) result(val)
+        integer(kind=i32), intent(in) :: x(:)
+        integer(kind=i32) :: val(size(x, kind=i64))
+        integer(kind=i64) :: n, nl, nr, il, ir, ii, ii_presorted
+        integer(kind=i32) :: left(size(x, kind=i64)/2_i64), right(size(x, kind=i64)/2_i64+1_i64)
+        n = size(x, kind=i64)
+        if (n <= isort_cutoff) then
+            val = isort3(x)
+        else
+            ii_presorted = 1_i64
+            do il=2_i64,n
+                if (x(il) < x(il-1_i64)) then !! out of place
+                    ii_presorted = il - 1_i64
+                    exit
+                end if
+            end do
+            if (il >= (n - 1_i64)) then
+                val = isort3(x)
+                return
+            end if
+            nl = (n-ii_presorted+1_i64)/2_i64
+            nr = (n-ii_presorted+1_i64) - nl
+            left = msort2(x(ii_presorted:ii_presorted+nl-1_i64))
+            right = msort2(x(ii_presorted+nl:n))
+            il = 1_i64
+            ir = 1_i64
+            ii = ii_presorted
+            do while ((il <= nl) .and. (ir <= nr))
+                if (left(il) <= right(ir)) then
+                    val(ii) = left(il)
+                    il = il + 1_i64
+                else
+                    val(ii) = right(ir)
+                    ir = ir + 1_i64
+                end if
+                ii = ii + 1_i64
+            end do
+            do while (il <= nl)
+                val(ii) = left(il)
+                il = il + 1_i64
+                ii = ii + 1_i64
+            end do
+            do while (ir <= nr)
+                val(ii) = right(ir)
+                ir = ir + 1_i64
+                ii = ii + 1_i64
+            end do
+            val = isort3(val)
+        end if
+    end function msort2
+
+    pure recursive function msort3(x) result(val)
+        integer(kind=i32), intent(in) :: x(:)
+        integer(kind=i32) :: val(size(x, kind=i64))
+        integer(kind=i64) :: n, nl, nr, il, ir, ii
+        integer(kind=i32) :: left(size(x, kind=i64)/2_i64), right(size(x, kind=i64)/2_i64+1_i64)
+        n = size(x, kind=i64)
+        if (n <= isort_cutoff) then
+            val = isort3(x)
+        else
+            nl = n/2_i64
+            nr = n - nl
+            left = msort3(x(1_i64:nl))
+            right = msort3(x(nl+1_i64:n))
+            il = 1_i64
+            ir = 1_i64
+            do ii=1_i64,n
+                if ((il <= nl) .and. (ir <= nr)) then
+                    if (left(il) <= right(ir)) then
+                        val(ii) = left(il)
+                        il = il + 1_i64
+                    else
+                        val(ii) = right(ir)
+                        ir = ir + 1_i64
+                    end if
+                else if (ir <= nr) then
+                    val(ii:n) = right(ir:nr)
+                    return
+                else
+                    val(ii:n) = left(il:nl)
+                    return
+                end if
+            end do
+        end if
+    end function msort3
 
     pure function isort(x) result(val)
         integer(kind=i32), intent(in) :: x(:)
@@ -138,6 +300,26 @@ contains
         end do
     end function isort2
 
+    pure subroutine isorts(x)
+        integer(kind=i32), intent(inout) :: x(:)
+        integer(kind=i64) :: n, i, j
+        integer(kind=i32) :: temp
+        n = size(x, kind=i64)
+        do i=2_i64,n
+            if (x(i) < x(i-1_i64)) then
+                temp = x(i)
+                loop_j: do j=i,2_i64,-1_i64
+                    if (temp < x(j-1_i64)) then
+                        x(j) = x(j-1_i64)
+                    else
+                        exit loop_j
+                    end if
+                end do loop_j
+                x(j) = temp
+            end if
+        end do
+    end subroutine isorts
+
     pure function isort3(x) result(val)
         integer(kind=i32), intent(in) :: x(:)
         integer(kind=i32) :: val(size(x, kind=i64))
@@ -148,13 +330,13 @@ contains
         do i=2_i64,n
             if (val(i) < val(i-1_i64)) then
                 temp = val(i)
-                do j=i,2_i64,-1_i64
+                loop_j: do j=i,2_i64,-1_i64
                     if (temp < val(j-1_i64)) then
                         val(j) = val(j-1_i64)
                     else
-                        exit
+                        exit loop_j
                     end if
-                end do
+                end do loop_j
                 val(j) = temp
             end if
         end do
--- src/tests/test_sorting.f90
+++ src/tests/test_sorting.f90
@@ -1,16 +1,18 @@
 program test_sorting
-use, non_intrinsic :: kinds, only: stdout, i32, i64, c_bool
+use, non_intrinsic :: kinds, only: stdout, i32, i64, c_bool, dp
 use, non_intrinsic :: random, only: random_uniform
-use, non_intrinsic :: sorting, only: is_sorted, qsort, isort, isort2, isort3, qsort2, msort
+use, non_intrinsic :: sorting, only: is_sorted, qsort, isort, isort2, isort3, qsort2, msort, msort2, msort3, qsort3, qsorts
 use, non_intrinsic :: timing, only: timer, tic, toc, get_elapsed
 implicit none
 
-    integer(kind=i64), parameter :: n = 10_i64
+    integer(kind=i64), parameter :: n = 32_i64
+    integer(kind=i32), parameter :: k_max = 1000_i32
 
-    integer(kind=i32) :: arr(n), arr_dupe(n), i
+    integer(kind=i32) :: arr(n), arr_dupe(n), i, k
     logical(kind=c_bool) :: sorted
     integer(kind=i32), allocatable :: x(:), ns(:), x2(:)
-    type(timer) :: bench(6)
+    type(timer) :: bench(7)
+    real(kind=dp) :: times(size(bench))
 
     call random_uniform(arr, -100_i32, 100_i32)
     arr_dupe = arr
@@ -43,9 +45,29 @@ implicit none
     write(unit=stdout, fmt='(a,l1,a,*(i4," "))') 'TEST_SORTING :: QSORT2 arr, sorted=',sorted,' :: ',arr
     arr = arr_dupe
 
+    arr = qsort3(arr_dupe)
+    sorted = is_sorted(arr)
+    write(unit=stdout, fmt='(a,l1,a,*(i4," "))') 'TEST_SORTING :: QSORT3 arr, sorted=',sorted,' :: ',arr
+    arr = arr_dupe
+
     arr = msort(arr_dupe)
     sorted = is_sorted(arr)
     write(unit=stdout, fmt='(a,l1,a,*(i4," "))') 'TEST_SORTING :: MSORT  arr, sorted=',sorted,' :: ',arr
+    arr = arr_dupe
+
+    arr = msort2(arr_dupe)
+    sorted = is_sorted(arr)
+    write(unit=stdout, fmt='(a,l1,a,*(i4," "))') 'TEST_SORTING :: MSORT2 arr, sorted=',sorted,' :: ',arr
+    arr = arr_dupe
+
+    arr = msort3(arr_dupe)
+    sorted = is_sorted(arr)
+    write(unit=stdout, fmt='(a,l1,a,*(i4," "))') 'TEST_SORTING :: MSORT3 arr, sorted=',sorted,' :: ',arr
+    arr = arr_dupe
+
+    call qsorts(arr)
+    sorted = is_sorted(arr)
+    write(unit=stdout, fmt='(a,l1,a,*(i4," "))') 'TEST_SORTING :: QSORTS arr, sorted=',sorted,' :: ',arr
     arr = arr_dupe
 
     if (allocated(ns)) deallocate(ns); allocate(ns(0))
@@ -55,43 +77,60 @@ implicit none
     do i=0_i32,20_i32
         ns = [ns, 2_i32**i]
     end do
-    ns = qsort(ns)
+    ns = msort(ns)
     write(unit=stdout, fmt='(a,*(i0," "))') 'TEST_SORTING :: ns: ',ns
 
     write(unit=stdout, fmt='(a)') 'TEST_SORTING :: benchmark qsort vs isort'
-    write(unit=stdout, fmt='(*(a16))') 'N Elements','| QSORT Time','| ISORT Time','| ISORT2 Time', &
-                                                    '| ISORT3 Time','| QSORT2 Time','| MSORT Time'
+    write(unit=stdout, fmt='(*(a16))') 'N Elements','| QSORT Time','| QSORT2 Time','| QSORT3 Time', &
+                                                    '| MSORT Time','| MSORT2 Time','| MSORT3 Time', &
+                                                    '| QSORTS Time'
     do i=1_i32,size(ns, kind=i32)
         if (allocated(x)) deallocate(x)
         if (allocated(x2)) deallocate(x2)
         allocate(x(ns(i)), x2(ns(i)))
-        call random_uniform(x, 0_i32, huge(1_i32))
-        x2 = x
-        call tic(bench(1))
-        x = qsort(x2)
-        call toc(bench(1))
-        if (.not.is_sorted(x)) error stop 'x not sorted after QSORT'
-        call tic(bench(2))
-        x = isort(x2)
-        call toc(bench(2))
-        if (.not.is_sorted(x)) error stop 'x not sorted after ISORT'
-        call tic(bench(3))
-        x = isort2(x2)
-        call toc(bench(3))
-        if (.not.is_sorted(x)) error stop 'x not sorted after ISORT2'
-        call tic(bench(4))
-        x = isort3(x2)
-        call toc(bench(4))
-        if (.not.is_sorted(x)) error stop 'x not sorted after ISORT3'
-        call tic(bench(5))
-        x = qsort2(x2)
-        call toc(bench(5))
-        if (.not.is_sorted(x)) error stop 'x not sorted after QSORT2'
-        call tic(bench(6))
-        x = msort(x2)
-        call toc(bench(6))
-        if (.not.is_sorted(x)) error stop 'x not sorted after MSORT'
-        write(unit=stdout, fmt='(i16,*(e16.6))') ns(i),get_elapsed(bench)
+        times = 0.0_dp
+        do k=1_i32,k_max
+            call random_uniform(x, 0_i32, huge(1_i32))
+            x2 = x
+            call tic(bench(1))
+            x = qsort(x)
+            call toc(bench(1))
+            if (.not.is_sorted(x)) error stop 'x not sorted after QSORT'
+            x = x2
+            call tic(bench(2))
+            x = qsort2(x)
+            call toc(bench(2))
+            if (.not.is_sorted(x)) error stop 'x not sorted after QSORT2'
+            x = x2
+            call tic(bench(3))
+            x = qsort3(x)
+            call toc(bench(3))
+            if (.not.is_sorted(x)) error stop 'x not sorted after QSORT3'
+            x = x2
+            call tic(bench(4))
+            x = msort(x)
+            call toc(bench(4))
+            if (.not.is_sorted(x)) error stop 'x not sorted after MSORT'
+            x = x2
+            call tic(bench(5))
+            x = msort2(x)
+            call toc(bench(5))
+            if (.not.is_sorted(x)) error stop 'x not sorted after MSORT2'
+            x = x2
+            call tic(bench(6))
+            x = msort3(x)
+            call toc(bench(6))
+            if (.not.is_sorted(x)) error stop 'x not sorted after MSORT3'
+            x = x2
+            call tic(bench(7))
+            call qsorts(x)
+            call toc(bench(7))
+            if (.not.is_sorted(x)) error stop 'x not sorted after QSORTS'
+            times = times + get_elapsed(bench)
+        end do
+        times = times/real(k_max, kind=dp)
+        times = times/minval(times)
+        write(unit=stdout, fmt='(i16,*(f16.2))') ns(i),times
     end do
 
     write(unit=stdout, fmt='(a,2(i0," "))') 'COMPLETE !! ',minval(x),maxval(x)
